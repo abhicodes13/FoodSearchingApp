082da8cde96dc57e7ddce087b33b28533662b51d
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-04 12:55:13 +0000

    Test

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -9,7 +9,6 @@ export default function AlertBox(){
   return(
     <Alert status='success'>
   <AlertIcon />
-  <AlertTitle>Student discount!</AlertTitle>
   <AlertDescription>Join now and get free trial for a month</AlertDescription>
 </Alert>
   )
