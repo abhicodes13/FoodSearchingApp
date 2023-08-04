270ee271c98b353b147748615632362360f72a49
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2023-08-04 13:08:09 +0000

    Added a CardBoard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import AlertBox from './components/AlertBox';
+import CardBoard from './components/CardBoard';
 
 
 export default function App() {
   return (
    <ChakraProvider>
       <AlertBox />
+     <CardBoard />
     </ChakraProvider>
   )
 }
